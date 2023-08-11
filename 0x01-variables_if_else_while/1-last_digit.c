894f1b535ca7c14a77f461c61a5102bcc87e8219
mouhibabdel <abdelmouhib202@gmail.com>
2023-08-11 14:24:39 -0700

    is the last digit 5 or 6 or 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 5)
 {
-printf("is %d and is greater than 5\n", n)l
+printf("is %d and is greater than 5\n", n);
 }
 if (n == 0)
 {
@@ -23,7 +23,7 @@ printf("is %d and is 0\n", n);
 }
 if (n > 6 && n != 0)
 {
-printf("is %d and and is less than 6 and not 0\n", n)
+printf("is %d and and is less than 6 and not 0\n", n);
 }
 return (0);
 }
