4527bc23cbc902e2bb608d61d71f3e0fb8946c18
mouhibabdel <abdelmouhib202@gmail.com>
2023-08-11 14:22:14 -0700

    is the last digit 5 or 6 or 0

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 5)
+{
+printf("is %d and is greater than 5", n)l
+}
+if (n == 0)
+{
+printf("is %d and is 0", n);
+}
+if (n > 6 && n != 0)
+{
+printf("is %d and and is less than 6 and not 0", n)
+}
+return (0);
+}
