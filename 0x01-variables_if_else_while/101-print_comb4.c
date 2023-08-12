318e119d9ebe4145674ef71cc3c6b0ca736319be
mouhibabdel <abdelmouhib202@gmail.com>
2023-08-11 18:43:43 -0700

    prints all possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: ALways 0 (Success)
+ */
+int m(void)
+{
+int n1 = 0;
+while (n1 <= 7)
+{
+int n2 = n1 + 1;
+while (n2 <= 8)
+{
+int n3 = n2 + 1;
+while (n3 <= 9)
+{
+putchar('0' + n1);
+putchar('0' + n2);
+putchar('0' + n3);
+if (n1 != 7 || n2 != 8 || n3 != 9)
+{
+putchar(',');
+putchar(' ');
+}
+n3++;
+}
+n2++;
+}
+n1++;
+}
+return 0;
+}
