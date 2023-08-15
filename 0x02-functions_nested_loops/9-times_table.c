c44f56fc57fbf576fbd8883c2fa76c3a0bd98892
mouhibabdel <abdelmouhib202@gmail.com>
2023-08-15 16:42:59 -0700

    9 times table, starting with 0.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,9 +11,16 @@ for (t = 0; t <= 9; t++)
 for (f = 0; f <= 9; f++)
 {
 r = t * f;
+if (r < 10)
+{
+putchar(r + '0');
+}
+else
+{
 _putchar(r / 10 + '0');
 _putchar(r % 10 + '0');
- if (f < 9)
+}
+if (f < 9)
 {
 _putchar(',');
 _putchar(' ');
