765bd6bc917ec62fabe83a5d9beb0e3abd0607ec
mouhibabdel <abdelmouhib202@gmail.com>
2023-08-15 17:18:06 -0700

    sum od teo numbs

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: int to be added to n2
+ * @b: int to be added to n1
+ * Return: sum of n1 and n2
+ */
+int add(int n1, int n2)
+{
+{
+int n1, n2, sum;
+sum = n1 + n2
+return (sum);
+}
