a31234032955770aa8799e71773e2c3b1cc219e4
mouhibabdel <abdelmouhib202@gmail.com>
2023-08-15 17:04:55 -0700

    9 times table, starting with 0.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,9 @@
 void times_table(void)
 {
 int t, f, r;
-for(t = 0; t <= 9; t++)
+for (t = 0; t <= 9; t++)
 {
-for(f = 0; f <= 9; f++)
+for (f = 0; f <= 9; f++)
 {
 r = t * f;
 if (r < 10)
@@ -23,8 +23,8 @@ _putchar(r + '0');
 else
 {
 _putchar(' ');
-_putchar(r / 10+ '0');
-_putchar(r % 10+ '0');
+_putchar(r / 10 + '0');
+_putchar(r % 10 + '0');
 }
 if (f < 9)
 {
