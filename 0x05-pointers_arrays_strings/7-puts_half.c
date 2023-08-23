e4c7632ea119664be00b4fe32fd38e1c87139a02
mouhibabdel <abdelmouhib202@gmail.com>
2023-08-22 17:14:31 -0700

    print the 3 half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - a function that prints half of a string
+ * if odd len, n = (length_of_the_string - 1) / 2
+ * @str: input
+ * Return: half of input
+ */
+void puts_half(char *str)
+{
+int n;
+int length = 0;
+while (*str != '\0')
+{
+length++;
+str++;
+}
+str -= length;
+n = (length ) / 2;
+while (str[n] != '\0')
+{
+putchar(str[n]);
+n++;
+}
+}
