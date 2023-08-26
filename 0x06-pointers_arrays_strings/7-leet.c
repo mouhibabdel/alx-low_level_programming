6ab4890c3cd62179ed4aa0392a496b1396e18a73
mouhibabdel <abdelmouhib202@gmail.com>
2023-08-25 17:14:17 -0700

    change letters by numb

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+char *leet(char *str)
+{
+int i, j;
+char n1[] = "aAeEoOtTlL";
+char n2[] = "4433007711";
+for(i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; str[j] != '\0'; j++)
+{
+if (str[i] == n1[j])
+{
+str[i] = n2[j];
+}
+}
+}
+return (str);
+}
