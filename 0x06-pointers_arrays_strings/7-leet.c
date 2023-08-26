9f427562bc90bc753ee6cd5f3ad86a8f6f20e5fc
mouhibabdel <abdelmouhib202@gmail.com>
2023-08-25 17:18:12 -0700

    change letters by numb

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,9 +9,9 @@ char *leet(char *str)
 int i, j;
 char n1[] = "aAeEoOtTlL";
 char n2[] = "4433007711";
-for(i = 0; str[i] != '\0'; i++)
+for (i = 0; str[i] != '\0'; i++)
 {
-for (j = 0; str[j] != '\0'; j++)
+for (j = 0; j < 10; j++)
 {
 if (str[i] == n1[j])
 {
