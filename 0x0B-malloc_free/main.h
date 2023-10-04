35a51d41f2be10e5957642a4fc470251d0de6ebc
mouhibabdel <abdelmouhib202@gmail.com>
2023-10-04 09:27:33 -0700

    updates

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+#endif
