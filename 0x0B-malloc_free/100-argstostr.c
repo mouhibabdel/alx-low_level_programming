5bcc17dfef72e253cff06c89e26acffdb424cf6c
mouhibabdel <abdelmouhib202@gmail.com>
2023-10-31 01:05:03 -0700

    function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * argstostr - Concatenates program arguments into a single string with newlines.
+ * argstostr - Concatenates program arguments into a string with newlines.
  * @ac: Number of program arguments.
- * @av: Array of program argument strings.
+ * @av: Array of argument strings.
  *
  * Description: Concatenates program arguments with newlines, returning a string.
- * Returns a pointer to the concatenated string or NULL on memory allocation failure.
+ *
+ * Return: Pointer to the concatenated string, or NULL on memory allocation failure.
  */
 char *argstostr(int ac, char **av)
 {
@@ -23,7 +24,7 @@ for (j = 0; av[i][j] != '\0'; j++)
 {
 total_length++;
 }
-total_length++; 
+total_length++;
 }
 concatenated = (char *)malloc(sizeof(char) * (total_length + 1));
 if (concatenated == NULL)
