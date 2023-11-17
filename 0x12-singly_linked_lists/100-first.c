e58d80e0b56e63679b262523700c31e54e7f3c34
mouhibabdel <abdelmouhib202@gmail.com>
2023-11-16 16:09:29 -0800

    print befor main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * print_message -  prints message before main
+ * Return: void
+ */
+void __attribute__((constructor)) print_message(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
